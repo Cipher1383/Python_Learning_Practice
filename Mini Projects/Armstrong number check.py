6e81ca00ce394edfc1e6632f907876c6273b1d81
Jayant <jayantmittal.1383@gmail.com>
2026-07-30 02:48:45 +0530

    New Project

---
+++ Mini Projects/Armstrong number check.py
@@ -0,0 +1,17 @@
+usr_inp = (input("Please Enter a number to check: "))
+proc_inp = int(usr_inp)
+inp_len = len(usr_inp)
+j= 0
+for i in range(inp_len):
+    rem = int(proc_inp % 10)
+   # print(rem)
+    j+=rem**inp_len
+    proc_inp /= 10
+
+if j == int(usr_inp):
+    print("The Entered number is an Armstrong Number!!")
+else:
+    print("The Entered number is NOT an Armstrong Number!!")
+
+
+
